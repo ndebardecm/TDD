40d9e32ec7a13f20cf4fbeb3b8f60399c2a577c4
nico <ndebard@ec-m.fr>
2015-01-11 20:05:31 +0100

    Chapter 8

--- src/main/java/fr/tp/Money.java
+++ src/main/java/fr/tp/Money.java
@@ -3,12 +3,22 @@ package fr.tp;
 /**
  * Created by nico on 11/01/2015.
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return money.amount == this.amount && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
