d3513219a0dc82651cfa6b55cb6d592e2dc74c5e
nico <ndebard@ec-m.fr>
2015-01-12 10:33:24 +0100

    Chapter 11

--- src/test/java/fr/tp/TestArithmetic.java
+++ src/test/java/fr/tp/TestArithmetic.java
@@ -19,8 +19,6 @@ public class TestArithmetic {
     public void testEquality() {
         Assert.assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         Assert.assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        Assert.assertTrue(Money.franc(5).equals(Money.franc(5)));
-        Assert.assertFalse(Money.franc(5).equals(Money.franc(6)));
         Assert.assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -35,10 +33,5 @@ public class TestArithmetic {
     public void testCurrency() {
         Assert.assertEquals("USD", Money.dollar(1).currency());
         Assert.assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        Assert.assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
