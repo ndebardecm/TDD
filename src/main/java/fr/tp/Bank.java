6bac2d836151e4867fadc7bc05a04aab7c90e360
nico <ndebard@ec-m.fr>
2015-01-12 10:58:43 +0100

    Chapter 12

--- src/main/java/fr/tp/Bank.java
+++ src/main/java/fr/tp/Bank.java
@@ -4,7 +4,7 @@ package fr.tp;
  * Created by nico on 12/01/2015.
  */
 public class Bank {
-    Money reduce(Expression source, String to){
+    Money reduce (Expression source, String to){
         return Money.dollar(10);
     }
 }
