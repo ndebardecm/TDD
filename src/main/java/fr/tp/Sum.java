5edc8ef87cde125f7719dd11149272d7f54608d8
nico <ndebard@ec-m.fr>
2015-01-13 19:10:36 +0100

    Chapter 16

--- src/main/java/fr/tp/Sum.java
+++ src/main/java/fr/tp/Sum.java
@@ -18,8 +18,12 @@ public class Sum implements Expression {
         return new Money(amount, to);
     }
 
-    public Expression plus(Expression addend){
-        return null;
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier),addend.times(multiplier));
     }
 
 }
